b3f6afc533387b4b6df81ae848e1d35561c77d9e
Franco42315 <leonardofranco3100@gmail.com>
2024-10-10 11:28:40 -0600

    add README

---
+++ src/main/java/com/example/Integradoraturismo/IntegradoraturismoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Integradoraturismo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegradoraturismoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegradoraturismoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Integradoraturismo/IntegradoraturismoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Integradoraturismo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegradoraturismoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
