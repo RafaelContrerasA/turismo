5af9b00769af3b7201f92130bf52ed75e6dfc84c
RafaelContrerasA <l20120970@morelia.tecnm.mx>
2024-10-11 06:52:59 -0600

    Modelo de Usuario y Administrador General

---
+++ src/main/java/com/example/Integradoraturismo/model/Usuario.java
@@ -0,0 +1,64 @@
+package com.example.Integradoraturismo.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String telefono;
+
+    @Column(name = "es_staff")
+    private boolean esStaff;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public boolean getEsStaff() {
+        return esStaff;
+    }
+
+    public void setEsStaff(boolean esStaff) {
+        this.esStaff = esStaff;
+    }
+}
