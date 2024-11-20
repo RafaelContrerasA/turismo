1cf2c49acaeed7fe8a76863d32d705e513a5ec7b
Rafael Alejandro Contreras Avalos <l20120970@morelia.tecnm.mx>
2024-11-20 06:38:02 -0600

    avance productos

--- src/main/java/com/example/Integradoraturismo/models/Producto.java
+++ src/main/java/com/example/Integradoraturismo/models/Producto.java
@@ -1,79 +1,30 @@
 package com.example.Integradoraturismo.models;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import java.util.List;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data // Genera automáticamente getters, setters, toString, equals, y hashCode
+@NoArgsConstructor // Genera un constructor vacío
+@AllArgsConstructor // Genera un constructor con todos los campos
 public class Producto {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;  // Cambiado a Long para consistencia.
+    private Long id; // Cambiado a Long para consistencia.
 
-    @Column(name = "name", nullable = false)
     private String nombre;
 
-    @Column(name = "descripcion", nullable = true, length = 255)
     private String descripcion;
 
-    @Column(name = "value", nullable = false)
     private float precio;
 
-    @Column(name = "inStock", nullable = false)
-    private boolean disponibilidad;
-
-    // Constructor vacío
-    public Producto() {}
-
-    // Constructor con parámetros
-    public Producto(String nombre, String descripcion, float precio, boolean disponibilidad) {
-        this.nombre = nombre;
-        this.descripcion = descripcion;
-        this.precio = precio;
-        this.disponibilidad = disponibilidad;
-    }
-
-    // Getters y Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
-    public float getPrecio() {
-        return precio;
-    }
-
-    public void setPrecio(float precio) {
-        this.precio = precio;
-    }
-
-    public boolean isDisponibilidad() {
-        return disponibilidad;
-    }
-
-    public void setDisponibilidad(boolean disponibilidad) {
-        this.disponibilidad = disponibilidad;
-    }
+    private int stock;
+    
+    @OneToMany(mappedBy = "producto", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ImagenProducto> imagenes;
 }
